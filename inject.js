3f8708ffecd1e6a591ffdcf0ae179864665e6081
Bogenbai Bayzharassov <thatoranzhevyy@gmail.com>
2023-05-01 01:32:56 +0600

    update adding to queue

--- inject.js
+++ inject.js
@@ -1,29 +1,20 @@
-setTimeout(() => telegramPosting.init(), 100);
+setTimeout(() => artstationExtention.init(), 100);
 
-const telegramPosting = {
-  firebaseConfig: {
-    apiKey: "AIzaSyAdfYrlyXBgVxWBc4GLUHhFpsolDofBNmI",
-    authDomain: "murraya-2f5d7.firebaseapp.com",
-    projectId: "murraya-2f5d7",
-    storageBucket: "murraya-2f5d7.appspot.com",
-    messagingSenderId: "1092792667601",
-    appId: "1:1092792667601:web:fabeea632642084a8464f0"
-  },
+
+const artstationExtention = {
+  headers: new Headers(),
   init() {
-    // Инициализация Firebase базы данных
-    firebase.initializeApp(this.firebaseConfig);
-    const firestoreDB = firebase.firestore();
-    this.artworkRef = firestoreDB.collection("artwork");
-
+    this.headers.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoib3JhbmdlIn0.e2eKjEmta0gVy5Y1rSTwC8ZXo1AWxLVzdYATZ50oSm4");
+    this.headers.append("Content-Type", "application/json");
     // Создание кнопки поста
     this.createButton();
   },
   createButton() {
-    $('.menu-level')
-      .append($("<li/>", {class: "main-menu-bar-it0em"})
+    $('.menu-level-1-buttons')
+      .append($("<li/>", {class: "main-menu-bar-item underline-none"})
         .append($("<a/>", {class: "main-menu-bar-link menu-item-telegram"})
           .append($("<span/>", {class: "bs-btn bs-btn-primary", text: "to telegram"}))));
-    $(".menu-item-telegram").bind("click", () => telegramPosting.clickToAddArtwork());
+    $(".menu-item-telegram").bind("click", () => this.clickToAddArtwork());
   },
   updateButton(setting) {
     const telegram_button = $('.menu-item-telegram').find('.bs-btn')
@@ -44,31 +35,44 @@ const telegramPosting = {
       log("Artwork hash not found!", "error");
       return;
     }
-    this.getArtworkByHash(artworkHash).then((artworkData) => {
-      if (artworkData) {
-        this.updateButton({color: "#fa0000", text: "element exist"});
-      } else {
-        this.addArtworkByHash(artworkHash)
-      }
-    })
+    await this.getArtworkByHash(artworkHash)
+      .then((artworkData) => {
+        if (!artworkData) {
+          this.addArtworkByHash(artworkHash)
+        }
+      })
   },
   async addArtworkByHash(artworkHash) {
-    const artwork = {planned: true, posted: false};
     try {
-      const docRef = await this.artworkRef.doc(artworkHash).set(artwork);
-      this.updateButton({color: "#21fa00", text: "success added"});
+      let requestOptions = {
+        method: 'POST',
+        headers: this.headers,
+        body: JSON.stringify({"hash": artworkHash})
+      };
+      fetch("https://orangedb.buhe.ga/hashes", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+          this.updateButton({color: "#21fa00", text: "success added"});
+        })
+        .catch(error => console.log('error', error));
     } catch (error) {
       log('Error Adding Artwork: ' + error, "error");
     }
-
-    return artwork;
   },
   async getArtworkByHash(artworkHash) {
     let artwork;
     try {
-      let doc = await this.artworkRef.doc(artworkHash).get();
-      if (doc.exists)
-        artwork = {id: doc.id, ...doc.data()};
+      await fetch("https://orangedb.buhe.ga/hashes?hash=eq." + artworkHash,
+        {method: 'GET', headers: this.headers}
+      )
+        .then(response => response.json())
+        .then(result => {
+          if (result.length !== 0) {
+            this.updateButton({color: "#fa0000", text: "element exist"});
+            artwork = result[0];
+          }
+        })
+        .catch(error => console.log('error', error));
     } catch (error) {
       log('Error in getting artwork: ' + error, "error");
     }
